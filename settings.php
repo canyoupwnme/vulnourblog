3c8958d7aaef01def3b8607b46ea86eca6156004
Sinan Şahin <bote.sinansahin@gmail.com>
2016-10-29 16:15:44 +0300

    Settings düzenlendi

--- settings.php
+++ settings.php
@@ -1,4 +1,9 @@
 <?php
-/**
-db bağlantı ayarları eklenecek.
- */
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "vulnourblog";
+$db_connection = mysql_connect($db_host, $db_user, $db_pass);
+if(! $db_connection) die("MySQL'e bağlanılamıyor");
+mysql_select_db($db_name, $db_connection) or die ("Veritabanına bağlanılamıyor");
+?>
